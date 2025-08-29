8742db67bcd819831a4612d00066b6e18e12eb10
Pranto78 <fshahriyar44@gmail.com>
2025-08-30 00:14:09 +0600

    Added the template string

---
+++ Template-string.js
@@ -0,0 +1,42 @@
+// const poet = 'vjksdgjkW
+// JKWKJGhjkg'
+
+
+// const poem = 'amm pata jora jora' + 
+// 'nfjkgwengjkengkjengkjngklnglk'+
+
+
+
+
+// use ``;
+// and also ${}; {Both use for dynamic interaction}
+
+
+
+
+const poem = `am pata jora 
+marbo chabuk HHHHHHH`;
+
+console.log(poem);
+
+
+function sum(num1,num2){
+
+    const result = num1+num2;
+    
+    const output = `sum of ${num1} and ${num2} is ${result}`;
+
+    console.log(output);
+}
+
+sum(10,20);
+
+
+
+const price =90;
+
+const discountedPrice = `Discounted price is ${price * 0.8}`;
+
+console.log(discountedPrice);
+
+
