bb9795a180f0891b1b81fec1f765a6098e4eb4d6
Pranto78 <fshahriyar44@gmail.com>
2025-08-30 00:00:13 +0600

    first commit

---
+++ default-parameter.js
@@ -0,0 +1,34 @@
+
+
+// For addition default value will be 0 
+function sum(num1=0,num2=0){
+    const total = num1+num2;
+
+    console.log(total);
+}
+
+
+// sum(30,40);
+
+// For multiply default value will be 1
+function multiply(num1=1,num2=1){
+    const total = num1*num2;
+
+    console.log(total);
+}
+
+
+multiply(10);
+
+
+// function multiply(num1,num2){
+//     const total = num1+ ' ' +num2;
+
+//     console.log(total);
+// }
+
+
+
+
+
+
